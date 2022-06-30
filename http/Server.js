259dd49f57eb0fcb1d6fb11fdcaaa99f635d6d61
Kashil <kashilrr@gamil.com>
2022-06-30 11:48:32 +0530

    29/06/22

--- http/Server.js
+++ http/Server.js
@@ -3,7 +3,7 @@ console.log(http);
 var server = http.createServer((request, response) => {
   response.setHeader("Content-type", "text/html");
   // res.writeHead(200, { "Content-Type": "text/html" });
-  response.write("<h1 style= text-align:center;>Hello Kashil</h1>");
+  response.write("<h1 style= text-align:center;><i>Hello Kashil</i></h1>");
   console.log(" request rec.. ");
   response.end();
 });
